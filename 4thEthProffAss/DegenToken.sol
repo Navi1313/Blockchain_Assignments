ccef480cd51352213888353018f20d4dea911a36
Navi <121182901+Navi1313@users.noreply.github.com>
2024-07-20 13:41:59 +0530

    Update DegenToken.sol

--- 4thEthProffAss/DegenToken.sol
+++ 4thEthProffAss/DegenToken.sol
@@ -1,14 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract DegenToken is ERC20 {
-
     address private owner;
-
-    mapping(address => string[]) private Assets;
-
+    string[6] private Bikes = ["Ducati", "Yamaha", "honda" ,"R15","Harley Davidson","Royal Enfield"];
+    uint[6] private price$ = [10000, 5000, 2000 ,1000 ,7000 ,6000];
+    
     constructor() ERC20("Degen", "DGN"){
         owner = msg.sender;
     }
@@ -18,45 +18,30 @@ contract DegenToken is ERC20 {
         _;
     }
 
-// 1) Minting Function :
-    function mintToken(address _to,uint _val) public onlyOwner{
+    function mint(address _to,uint _val) public onlyOwner{
         _mint(_to,_val);
     }
 
-
-// 2) Transfer function  :
-    function transferToken(address _to, uint _val) public  {
-
-        require(balanceOf(msg.sender) >= _val , "Not enough Balance ");
-        _transfer(msg.sender, _to, _val);
-
-    }
-
-// 3) Redeem function :
-    function redeemToken(string memory _itemname) public {
-
-        uint price = 100;
-        _burn(msg.sender, price);
-        Assets[msg.sender].push(_itemname);
-
-    }
-
-
-// 4) Checking token balance
-    function getBalance(address account) public view returns (uint256) {
-        return balanceOf(account);
-    }
-
-
-// 5) Burning tokens
-    function burnToken(uint256 _val) public {
-        require(balanceOf(msg.sender) >= _val, "Insufficient balance to burn");
+    function burn(uint _val) public {
         _burn(msg.sender, _val);
     }
 
+    function redeem(uint _item) public {
 
-    function Asset(address _adr) public view returns (string[] memory) {
-        return Assets[_adr];
+        require(_item-1 < Bikes.length, "Item index out of bounds");
+        uint price = price$[_item-1];
+        require(balanceOf(msg.sender) >= price, "Insufficient balance to redeem");
+        
+        _burn(msg.sender, price);
+        console.log("You redeemed Tokens for the Bike : ", Bikes[_item-1]);
     }
 
+    function transferTo(address _to, uint _val) public  {
+        _transfer(msg.sender, _to, _val);
+    }
+
+    function getBalance(address _addr) external view returns(uint){
+        return balanceOf(_addr);
+    }
+    
 }
