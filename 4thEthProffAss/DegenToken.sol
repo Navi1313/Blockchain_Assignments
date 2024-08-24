2731b2c54d91b69317ef85e67ea2cf48c4b30eea
Navi <121182901+Navi1313@users.noreply.github.com>
2024-08-24 14:21:46 +0530

    Update DegenToken.sol

--- 4thEthProffAss/DegenToken.sol
+++ 4thEthProffAss/DegenToken.sol
@@ -6,42 +6,50 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract DegenToken is ERC20 {
     address private owner;
-    string[6] private Bikes = ["Ducati", "Yamaha", "honda" ,"R15","Harley Davidson","Royal Enfield"];
-    uint[6] private price$ = [10000, 5000, 2000 ,1000 ,7000 ,6000];
-    
-    constructor() ERC20("Degen", "DGN"){
+    string[6] private Bikes = ["Ducati", "Yamaha", "honda", "R15", "Harley Davidson", "Royal Enfield"];
+    uint256[6] private prices = [10000, 5000, 2000, 1000, 7000, 6000];
+    mapping(address => string[]) private OwnedBikes;
+
+    constructor() ERC20("Degen", "DGN") {
         owner = msg.sender;
     }
 
-    modifier onlyOwner{
-        require(owner==msg.sender,"Only owner has access");
+    modifier onlyOwner {
+        require(owner == msg.sender, "Only owner has access");
         _;
     }
 
-    function mint(address _to,uint _val) public onlyOwner{
-        _mint(_to,_val);
+    function mint(address _to, uint _val) public onlyOwner {
+        _mint(_to, _val);
     }
 
     function burn(uint _val) public {
         _burn(msg.sender, _val);
     }
 
-    function redeem(uint _item) public {
-
-        require(_item-1 < Bikes.length, "Item index out of bounds");
-        uint price = price$[_item-1];
-        require(balanceOf(msg.sender) >= price, "Insufficient balance to redeem");
-        
-        _burn(msg.sender, price);
-        console.log("You redeemed Tokens for the Bike : ", Bikes[_item-1]);
-    }
-
-    function transferTo(address _to, uint _val) public  {
-        _transfer(msg.sender, _to, _val);
-    }
-
-    function getBalance(address _addr) external view returns(uint){
+    function getBalance(address _addr) external view returns (uint) {
         return balanceOf(_addr);
     }
-    
+
+    function redeem(uint _item) public {
+        require(_item - 1 < Bikes.length && _item - 1 >= 0, "Item index out of bounds");
+
+        uint price = prices[_item - 1];
+        require(balanceOf(msg.sender) >= price, "Not sufficient amount to do a transaction");
+
+        _burn(msg.sender, price);
+        OwnedBikes[msg.sender].push(Bikes[_item - 1]);
+
+        console.log("You redeemed Tokens for the Bike: ", Bikes[_item - 1]);
+    }
+
+    function getOwnedBikes(address _owner) public view returns (string[] memory) {
+        return OwnedBikes[_owner];
+    }
+
+    function transferTo(address _to, uint _val) public {
+        _transfer(msg.sender, _to, _val);
+    }
 }
+
+
